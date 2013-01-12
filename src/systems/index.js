4254ab91a2103b7eef3374e14b42128632b202c5
Michal Miskernik <michal.miskernik@gmail.com>
2013-01-12 21:10:03 +0100

    export just array of systems

--- src/systems/index.js
+++ src/systems/index.js
@@ -1,19 +1,7 @@
-var systems = {};
-
-[
-  'collisions', 'drawing', 'gravity', /*'input',*/
-  'movement', 'solid'
-].forEach(function(name) {
-  systems[name] = require('./' + name);
-});
-
-module.exports = function setup(manager) {
-  var result = {};
-
-  for (var name in systems) {
-    switch (name) {
-      case 'solid':
-        result[name] = new systems[name](manager, )
-    }
-  }
-};
+module.exports =
+  [
+    'collisions', 'drawing', 'gravity', /*'input',*/
+    'movement', 'solid'
+  ].map(function(name) {
+    return require('./' + name);
+  });
