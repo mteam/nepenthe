54a171b2ff84614e2a2ff54d16c840fb0d4ee20d
Michal Miskernik <michal.miskernik@gmail.com>
2013-02-09 12:28:20 +0100

    menu stage

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+var Container = require('stages').Container,
+    Menu = require('menu');
+
 function Game() {
   this.container = null;
 }
 
 module.exports = Game;
 
-Game.prototype.init = function(container) {
-  this.container = container;
+Game.prototype.init = function(element) {
+  this.container = new Container(element);
 };
 
 Game.prototype.start = function() {
   console.log('game started');
+
+  this.container.use(new Menu);
 };
